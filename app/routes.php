2d541254fc751f00f8a0b732b050dface1a75a5c
nstirnemann <nstirnemann@zennovia.com>
2021-02-17 20:45:52 -0300

    Sacar controlador de prueba

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,4 @@
 use App\Controllers\HelloController;
 use App\Controllers\SpotifyController;
 
-$app->get('/hello/{name}', 'HelloController:home');
 $app->get('/api/v1/albums', 'SpotifyController:get');
